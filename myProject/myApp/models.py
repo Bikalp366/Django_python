d2c393c2f0d925e9a792f7290217897f06d7a8ae
Bikalp366 <bikalplama20@gmail.com>
2026-01-28 13:35:05 +0545

    pushing again

---
+++ myProject/myApp/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+# class Customer(models.Model):
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     phone_number = models.CharField(max_length=15, null=True, blank=True)
+#     address = models.TextField(null=True, blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
---
+++ myProject/myApp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from . import models
+# Create your views here.
+
+def home(request):
+    products = models.Product.objects.all()
+   # customers = models.Customer.objects.all()
+    return render(request, 'home.html', {'products': products})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
